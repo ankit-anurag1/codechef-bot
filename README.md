8ea989c0985ef974a12329ae86ddf94907f6fda6
Ankit Anurag <ankit.anurag23@gmail.com>
2020-05-05 08:18:09 +0530

    added comments; modified readme and gitignore; added requirements.txt

--- README.md
+++ README.md
@@ -3,4 +3,9 @@
 ### Python script for automated directory creation  
   
     
-Uses the 'beautiful soup' and 'requests' modules to extract problem codes from the URL and 'subprocess' module to create corresponding directories.
+Uses the 'beautiful soup' and 'requests' modules to extract problem codes from the URL and 'subprocess', 'os' and 'sys' module to create corresponding directories and files. **The script is environment-specific.**    
+  
+   
+I have saved the python file in my home directory to save myself the efforts of typing the entire directory path.  
+
+For my system environment, the required command is - **python3 codechef_env.py *Event URL*.**
--- codechef_env.py
+++ codechef_env.py
@@ -5,29 +5,34 @@ import os
 import shutil
 import subprocess
 
+# check if link has been provided
 if len(sys.argv) != 2:
   print("Unexpected number of command line arguments.")
   sys.exit()
 else:
   url = sys.argv[1]
+
+# access the webpage
 try:
   res = requests.get(url)
 except:
   sys.exit("Could not access", url, end=".\n")
 
+# using beautifulsoup to parse the html
 res = bs(requests.get(url).text, features="html.parser")
+
+# extracting the table containing problems
 try:
   res = res.find(class_="dataTable").find("tbody")
 except:
   sys.exit("Could not find class \"dataTable\" and/or \"tbody\".")
 
-  
+# extracting individual problems
 problems = res.find_all(class_="problemname")
 if(len(problems) == 0):
   sys.exit("Could not find class \"problemname\".")
 
-
-
+# extracting problem code from problem-url
 try:   
   problem_code = [
     problem.find(name="a")["href"][problem.find(name="a")["href"].rindex('/')+1:].lower()+".cpp"
@@ -36,21 +41,37 @@ try:
 except:
   sys.exit("Could not access problem codes.")
 
+
+# my directory structure :
+# home -> Users -> ankitanurag -> Desktop -> competitive ->
+# codechef -> event_folder
+
 target_dir = r"/Users/ankitanurag/Desktop/competitive"
-folder_name = 'codechef/'+url[url.rindex('/')+1:].lower()
+
+# extract event code from page url - ignores query params
+folder_name = 'codechef/'+url[url.rindex('/')+1:url.index('?')].lower()
+
 
 os.chdir(target_dir)
 
+# create directory if it does not exist
 if not os.path.exists(folder_name):
   subprocess.call(['mkdir', folder_name])
 else:
-  sys.exit(f"\"{folder_name}\" already exists.")
+  print(f"\"{folder_name}\" already exists.")
+  ow = input("Overwrite contents of folder? (y/n) ")
+  if ow != 'y' or ow != 'Y':
+    sys.exit("Aborting...")
 
+# redundant safety check for switching to created event folder
 if os.path.exists(folder_name):
   os.chdir(target_dir+'/'+folder_name)
 else:
   sys.exit(f"\"{target_dir}/{folder_name}\" not found.")
 
+# creating cpp files for individual problems
+# template directory structure:
+# home -> Users -> ankitanurag -> Desktop -> competitive -> template_cpp.cpp
 try:
   template = target_dir+r"/template_cpp.cpp"
   for problem in problem_code:
@@ -60,6 +81,7 @@ except:
 
 print(f"Successfully created {len(problem_code)} files.")
 
+# for VSCode, if added to path
 try:
   subprocess.call(['code', './.'])
 except:
